01a18a260a083d219abc6103c9e07b30cf0460be
Roksolyanka <roksolyanakushnir@gmail.com>
2023-07-03 16:34:27 +0300

    corrected

--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { App } from 'components/APP/App';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-    />
   </React.StrictMode>
 );
